87019dc6af4151a85fd0ae1ec9d2db70a60b5c1c
akshathsk <akshath.sk@gmail.com>
2022-11-28 16:17:48 -0600

    Email integration with slot booking

--- src/main/java/com/uiuc/fp/domain/event/UserSlotEventHandler.java
+++ src/main/java/com/uiuc/fp/domain/event/UserSlotEventHandler.java
@@ -2,14 +2,20 @@ package com.uiuc.fp.domain.event;
 
 import com.uiuc.fp.domain.UserSlot;
 import com.uiuc.fp.exception.ValidationException;
+import com.uiuc.fp.util.email.dto.EmailDetails;
+import com.uiuc.fp.util.email.service.EmailService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.persistence.PostPersist;
 import javax.persistence.PrePersist;
-import javax.persistence.PreRemove;
 import javax.persistence.PreUpdate;
 
 @Slf4j
 public class UserSlotEventHandler {
+
+  @Autowired
+  private EmailService emailService;
 
   private static void validateSlotTime(UserSlot userSlot) {
     if (userSlot.getStartTime() != null
@@ -26,9 +32,28 @@ public class UserSlotEventHandler {
    */
   @PrePersist
   @PreUpdate
-  @PreRemove
-  private void beforeAnyUpdate(UserSlot userSlot) {
+  private void beforeUpdate(UserSlot userSlot) {
     log.debug("Captured userSlot update");
     validateSlotTime(userSlot);
   }
+
+  @PostPersist
+  private void afterInsert(UserSlot userSlot) {
+    if (userSlot.getIsBooked() != null && userSlot.getIsBooked()) {
+      sendSlotBookedEmail(userSlot);
+    }
+  }
+
+  private void sendSlotBookedEmail(UserSlot userSlot) {
+    try {
+      EmailDetails details = new EmailDetails();
+      details.setSubject("[AppName] : Your session has been confirmed!");
+      details.setMsgBody(emailService.getSlotBookingEmailContent(userSlot));
+      details.setRecipient(userSlot.getBookedByUser().getEmail() + "," + userSlot.getUser().getEmail());
+      emailService.sendMail(details);
+    } catch (Exception e) {
+      log.info(e.getMessage(), e);
+    }
+  }
+
 }
--- src/main/java/com/uiuc/fp/util/email/service/EmailService.java
+++ src/main/java/com/uiuc/fp/util/email/service/EmailService.java
@@ -1,14 +1,27 @@
 package com.uiuc.fp.util.email.service;
 
+import com.uiuc.fp.domain.UserSlot;
 import com.uiuc.fp.util.email.dto.EmailDetails;
+import freemarker.template.Configuration;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+
+import javax.mail.internet.MimeMessage;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
+@Slf4j
 public class EmailService implements IEmailService {
+
+  @Autowired
+  Configuration fmConfiguration;
 
   @Autowired
   private JavaMailSender javaMailSender;
@@ -16,19 +29,52 @@ public class EmailService implements IEmailService {
   @Value("${spring.mail.username}")
   private String sender;
 
+  @Value("${mail.enable}")
+  private boolean enableEmail;
+
   public String sendMail(EmailDetails details) {
 
-    try {
-      SimpleMailMessage mailMessage
-              = new SimpleMailMessage();
-      mailMessage.setFrom(sender);
-      mailMessage.setTo(details.getRecipient().trim().split(","));
-      mailMessage.setText(details.getMsgBody());
-      mailMessage.setSubject(details.getSubject());
-      javaMailSender.send(mailMessage);
-      return "Mail Sent Successfully...";
-    } catch (Exception e) {
-      return "Error while Sending Mail";
+    if (!enableEmail) {
+      return "Emails are disabled";
     }
+
+    MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+    try {
+      MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+      mimeMessageHelper.setSubject(details.getSubject());
+      mimeMessageHelper.setFrom(sender);
+      mimeMessageHelper.setTo(details.getRecipient().split(","));
+      mimeMessageHelper.setText(details.getMsgBody(), true);
+      javaMailSender.send(mimeMessageHelper.getMimeMessage());
+    } catch (Exception e) {
+      log.info(e.getMessage());
+      return "Failed to send email.";
+    }
+    return "Email sent successfully!";
+
   }
+
+  public String getSlotBookingEmailContent(UserSlot userSlot) {
+
+    Map<String, Object> model = new HashMap<>();
+    model.put("student", userSlot.getBookedByUser().getUserName());
+    model.put("tutor", userSlot.getUser().getUserName());
+    model.put("slotDate", userSlot.getSlotDate().toString());
+    model.put("startTime", userSlot.getStartTime().toString());
+    model.put("endTime", userSlot.getEndTime().toString());
+    model.put("email", userSlot.getUser().getEmail());
+    model.put("phone", userSlot.getUser().getPhoneNumber());
+    model.put("adminEmail", sender);
+    model.put("dateNow", LocalDate.now().toString());
+
+    StringBuffer content = new StringBuffer();
+    try {
+      content.append(FreeMarkerTemplateUtils.processTemplateIntoString(fmConfiguration.getTemplate("email.ftlh"), model));
+    } catch (Exception e) {
+      log.info(e.getMessage());
+    }
+    return content.toString();
+
+  }
+
 }
