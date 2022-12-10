7b35f68220f332199f4ac0ee4d0e46455817433a
akshathsk <akshath.sk@gmail.com>
2022-12-10 16:15:29 -0600

    POST Tag updated

--- src/main/java/com/uiuc/fp/controllers/TagController.java
+++ src/main/java/com/uiuc/fp/controllers/TagController.java
@@ -1,8 +1,11 @@
 package com.uiuc.fp.controllers;
 
+import com.uiuc.fp.domain.TagDto;
 import com.uiuc.fp.service.TagService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +21,10 @@ public class TagController {
   @GetMapping("/v2/tags")
   Object getTags() {
     return tagService.getAllTags();
+  }
+
+  @PostMapping(path = "/v2/tags")
+  public Object create(@RequestBody TagDto tagDto) {
+      return tagService.saveAllTags(tagDto);
   }
 }
--- src/main/java/com/uiuc/fp/service/TagService.java
+++ src/main/java/com/uiuc/fp/service/TagService.java
@@ -1,8 +1,13 @@
 package com.uiuc.fp.service;
 
+import com.uiuc.fp.domain.Tag;
+import com.uiuc.fp.domain.TagDto;
+import com.uiuc.fp.domain.Topic;
 import com.uiuc.fp.repository.TagRepository;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TagService {
@@ -15,5 +20,16 @@ public class TagService {
 
   public Object getAllTags() {
     return tagRepository.findAll();
+  }
+
+  public Object saveAllTags(TagDto tagDto) {
+    List<Tag> tags = (List<Tag>) tagRepository.findAllById(tagDto.getTagIds());
+    tags.forEach(tag -> {
+      Topic topic = new Topic();
+      topic.setTopicId(tagDto.getTopicId());
+      tag.getTopics().add(topic);
+    });
+
+    return tagRepository.saveAll(tags);
   }
 }
